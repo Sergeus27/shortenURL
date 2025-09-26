c780f14538ad0ada2b83e8b0e90c5d2adde53deb
Sergeus27 <7vgy@mail.ru>
2025-09-27 00:47:01 +0300

    JSON decoding

--- main.go
+++ main.go
@@ -1,19 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"log"
 	"net/http"
 )
 
+type ShortenRequest struct {
+	URL string `json:url`
+}
+
 func main() {
 
 	shortenHandler := func(w http.ResponseWriter, req *http.Request) {
 		if req.Method == "POST" {
-			io.WriteString(w, "OK\n")
+			var shortenRequest ShortenRequest
+			if err := json.NewDecoder(req.Body).Decode(&shortenRequest); err != nil {
+				http.Error(w, "Invalid JSON", http.StatusBadRequest)
+				return
+			}
+			log.Printf("URL from shorten handle - %s", shortenRequest.URL)
 		} else {
 			http.Error(w, "must be POST", http.StatusNotFound)
 		}
+
 	}
 
 	http.HandleFunc("/api/shorten", shortenHandler)
