55c336197afae5ee82b91752578fb3d84ad8b3de
Sergeus27 <7vgy@mail.ru>
2025-10-20 18:07:51 +0300

    добавил возврат короткой строки в JSON формате(jsonResponse) и создал мапу (urlStore)

--- main.go
+++ main.go
@@ -7,31 +7,49 @@ import (
 	"net/http"
 )
 
+var urlStore = make(map[string]string)
+
 type ShortenRequest struct {
-	URL string `json:url`
+	URL string `json:"url"`
+}
+
+type ShortenResponse struct {
+	ShortID string `json:"short_id"`
 }
 
 func main() {
-	log.Printf("ID:%s", generateShortID())
-	shortenHandler := func(w http.ResponseWriter, req *http.Request) {
-		if req.Method == "POST" {
-			var shortenRequest ShortenRequest
-			//объявляется error создается декодер и используется метод decode из этого декодера в который передаем ссылку на переменную для сохранения декодированного Body
-			decoder := json.NewDecoder(req.Body)
-			if err := decoder.Decode(&shortenRequest); err != nil {
-				http.Error(w, "Invalid JSON", http.StatusBadRequest)
-				return
-			}
-			log.Printf("URL from shorten handle - %s", shortenRequest.URL)
-		} else {
-			http.Error(w, "must be POST", http.StatusNotFound)
-		}
-
-	}
+	// log.Printf("ID:%s", generateShortID())
 	//почитать как работает http сервер(жизненный цикл)
 	http.HandleFunc("/api/shorten", shortenHandler)
 	err := http.ListenAndServe(":8080", nil)
 	log.Fatal(err)
+
+}
+
+func shortenHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method == "POST" {
+		var shortenRequest ShortenRequest
+		//объявляется error создается декодер и используется метод decode из этого декодера в который передаем ссылку на переменную для сохранения декодированного Body
+		decoder := json.NewDecoder(req.Body)
+		if err := decoder.Decode(&shortenRequest); err != nil {
+			http.Error(w, "Invalid JSON", http.StatusBadRequest)
+			return
+		}
+
+		log.Printf("URL from shorten handle - %s", shortenRequest.URL)
+
+		shortID := generateShortID()
+		response := ShortenResponse{ShortID: shortID}
+		jsonResponse, _ := json.Marshal(response)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(jsonResponse)
+
+		urlStore[shortID] = shortenRequest.URL
+		log.Printf("создал добавил в мапу urlStore с ключем: %s и значением: %s", shortID, urlStore[shortID])
+	} else {
+		http.Error(w, "must be POST", http.StatusNotFound)
+	}
+
 }
 
 func generateShortID() string {
